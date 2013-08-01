eab36e07a9062074ba6e9f1359ab997f85d9fa54
Cengizhan Paşaoğlu <cengizhanpasaoglu@gmail.com>
2013-08-02 00:38:13 +0300

    Indent size set.

--- include/sigar_cpu.h
+++ include/sigar_cpu.h
@@ -7,15 +7,15 @@ class sigarCPU
 {
 private:
 	sigar_uint64_t
-		user, 
-    	sys,
-        nice,
-        idle,
-        wait,
-        irq,
-        soft_irq,
-        stolen,
-        total;
+	user, 
+	sys,
+	nice,
+	idle,
+	wait,
+	irq,
+	soft_irq,
+	stolen,
+	total;
 public:
 	SIGAR_DECLARE(int) getCPU(sigar &,sigarCPU &);
 };
--- include/sigar_cpu_info.h
+++ include/sigar_cpu_info.h
@@ -7,22 +7,22 @@ class sigarCPUInfo
 {
 private:
 	char vendor[128];
-    char model[128];
-    int mhz;
-    int mhz_max;
-    int mhz_min;
-    sigar_uint64_t cache_size;
-    int total_sockets;
-    int total_cores;
-    int cores_per_socket;
+	char model[128];
+	int mhz;
+	int mhz_max;
+	int mhz_min;
+	sigar_uint64_t cache_size;
+	int total_sockets;
+	int total_cores;
+	int cores_per_socket;
 };
 
 class sigarCPUInfoList
 {
 private:
 	unsigned long number;
-    unsigned long size;
-    sigarCPUInfo *data;
+	unsigned long size;
+	sigarCPUInfo *data;
 public:
 	SIGAR_DECLARE(int) getCPUInfoList(sigar &,sigarCPUInfoList &);
 	SIGAR_DECLARE(int) destroyCPUInfoList(sigar &,sigarCPUInfoList &);
